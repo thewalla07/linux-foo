8fbeeb38295ff509a615d8c764b0f074b009cab8
thewalla07 <thewalla07@gmail.com>
2016-11-09 14:02:34 +0000

    Added updates for Python3 compatibility

--- .short.pwd.py
+++ .short.pwd.py
@@ -1,13 +1,13 @@
 import os
-from commands import getoutput
-from socket import gethostname
-hostname = "this_machine"
+#from commands import getoutput
+#from socket import gethostname
+#hostname = "this_machine"
 #hostname = gethostname()
 username = os.environ['USER']
 pwd = os.getcwd()
 homedir = os.path.expanduser('~')
 pwd = pwd.replace(homedir, '~', 1)
-if len(pwd) > 30:
-    pwd = pwd[:7]+'...'+pwd[-15:] # first 7 chars+last 15 chars
-#print '[%s@%s:%s] ' % (username, hostname, pwd)
-print '%s:%s ' % (username, pwd)
+if len(pwd) > 19:
+    pwd = pwd[:6]+'...'+pwd[-10:] # first 7 chars+last 15 chars
+#print(username+"@"+hostname+":"+pwd)
+print(username+":"+pwd+" $ ")
