8435951bf67b14da23074a04fb3f5bbe132938e0
Michael Wall <thewalla07@gmail.com>
2026-08-15 19:59:37 +0100

    fix: more flutter config

--- dotfiles/.config/zsh/export.zsh
+++ dotfiles/.config/zsh/export.zsh
@@ -1,2 +1,7 @@
 export EDITOR=nvim
+export PATH="$HOME/develop/flutter/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
 
