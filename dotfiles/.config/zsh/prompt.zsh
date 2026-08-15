ef28f50ced3e5123a9788931545dc86ee66ad193
Michael Wall <thewalla07@gmail.com>
2026-08-15 19:59:14 +0100

    fix: hostname showing network name

--- dotfiles/.config/zsh/prompt.zsh
+++ dotfiles/.config/zsh/prompt.zsh
@@ -7,5 +7,5 @@ COLOR_DIR='%F{197}'
 COLOR_GIT='%F{39}'
 NEWLINE=$'\n'
 setopt PROMPT_SUBST
-export PROMPT='${COLOR_USR}%n@%M ${COLOR_DIR}%d ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}${NEWLINE}%% '
+export PROMPT='${COLOR_USR}%n@%m ${COLOR_DIR}%d ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}${NEWLINE}%% '
 
